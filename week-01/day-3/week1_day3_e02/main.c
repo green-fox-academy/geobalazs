4314e1fe665a102707069147a0f2707386409186
geobalazs <balazskissgeo@gmail.com>
2017-10-05 23:30:42 +0200

    some comment

---
+++ week-01/day-3/week1_day3_e02/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int b = 100 - 7;
+	// make it smaller by 7
+    printf("%d", b);
+    return 0;
+}
---
+++ week-01/day-3/week1_day3_e05/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int e = 8 * 8 * 8;
+	// please cube of e's value
+    printf("%d" , e);
+    return 0;
+}
