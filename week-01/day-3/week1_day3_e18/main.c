5aac15d0ec8a7a719f4ad3f8ff117f56130ccab9
geobalazs <balazskissgeo@gmail.com>
2017-10-06 11:32:29 +0200

    if else statement added

--- week-01/day-3/week1_day3_e18/main.c
+++ week-01/day-3/week1_day3_e18/main.c
@@ -7,7 +7,10 @@ int main()
     uint16_t number = 254;
 	// if number can't be represented with 8bits
 	// print 'longer than 8bits' otherwise print 'ok'
-    printf("Hooray!\n");
+    if( number > 256 ){
+        printf("longer than 8bits");
+    } else{
+        printf("ok");
     }
     return 0;
 }
