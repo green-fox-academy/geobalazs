cd36bdadb97dea40fbc64de0f6082afb4d4ccc00
geobalazs <balazskissgeo@gmail.com>
2017-10-06 13:08:37 +0200

    remainder calc finished week1day3e19

---
+++ week-01/day-3/week1_day3_e19/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 654;
+	uint64_t b = 654987312;
+	int remainder = b/a;
+	if( remainder = 3){
+        printf("Hello world!\n");
+	}
+	// Tell if the reminder of b devided by a is 3
+    return 0;
+}
