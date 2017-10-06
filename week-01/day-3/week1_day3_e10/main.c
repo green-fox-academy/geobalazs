771236b86a42ba1168b90bf3d6b61f6867629d44
geobalazs <balazskissgeo@gmail.com>
2017-10-06 11:05:45 +0200

    if statement corrected and else is added

--- week-01/day-3/week1_day3_e10/main.c
+++ week-01/day-3/week1_day3_e10/main.c
@@ -6,8 +6,10 @@ int main()
     int j1 = 10;
 	int j2 = 3;
 	// tell if j1 is higher than j2 squared and smaller than j2 cubed
-	if(j2*j2*j2 > j1 > j2*j2){
+	if(j1 > j2*j2 && j1 < j2*j2*j2 ){
     printf("Hello world!\n");
+    } else {
+        printf("try again");
     }
     return 0;
 }
