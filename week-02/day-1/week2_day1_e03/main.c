d29fcac39aaf57f89faaf3e482be6ab099c2365a
geobalazs <balazskissgeo@gmail.com>
2017-10-09 15:36:17 +0200

    printer function with user input with more text week2day1e03

--- week-02/day-1/week2_day1_e03/main.c
+++ week-02/day-1/week2_day1_e03/main.c
@@ -11,13 +11,16 @@ void printer();
 
 int main()
 {
+    printf("Give three number and divide them with space or enter:\n");
     scanf("%d %d %d",&num1,&num2,&num3);
+    printf("\n");
     printer(num1,num2,num3);
     return 0;
 }
 
 void printer(int num1, int num2, int num3){
-        printf("%d\n" , num1);
-        printf("%d\n" , num2);
-        printf("%d\n" , num3);
+        printf("Your numbers:\n");
+        printf("%d\n\n" , num1);
+        printf("%d\n\n" , num2);
+        printf("%d\n\n" , num3);
 }
