5c249c19843d7047b92c2bd9d6b42c5a820aaa2a
geobalazs <balazskissgeo@gmail.com>
2017-10-09 16:33:36 +0200

    greet function with str variables week2day1e04

---
+++ week-02/day-1/week2_day1_e04/GFX.h
@@ -0,0 +1,4 @@
+char str1[] = "Greenfox";
+char str2[] = "Bluefox";
+char str3[] = "Blackfox";
+
---
+++ week-02/day-1/week2_day1_e04/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "GFX.h"
+
+// define a variable and assign the value `Greenfox` to it in a .h file
+// create a function called `greet` that greets it's input parameter
+// define more names to greet, call the function multiple times
+
+void greet(str1 , str2 , str3);
+
+int main()
+{
+    greet(str1 , str2 , str3);
+
+    return 0;
+}
+void greet(str1 , str2 , str3){
+
+    printf("Hello %s!\n" , str1);
+    printf("Hello %s!\n" , str2);
+    printf("Hello %s!\n" , str3);
+
+}
