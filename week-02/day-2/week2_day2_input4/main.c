893c68ca4c6f99b1d31ede78c487e71848c398f9
geobalazs <balazskissgeo@gmail.com>
2017-10-10 14:30:15 +0200

    scanf and print is added to function week2_day2_input4

---
+++ week-02/day-2/week2_day2_input4/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int num = 0;
+void func();
+
+int main()
+{
+    //write a void function which asks for a number with scanf and print it out with printf,
+    //then asks for a name with gets, and print it out with puts
+    //you will notice something is wrong
+    //try to solve the problem
+
+    func();
+
+    return 0;
+}
+
+void func(){
+        printf("Please type a number:\n");
+        scanf("%d" , &num);
+        printf("Your number is: %d" , num);
+}
