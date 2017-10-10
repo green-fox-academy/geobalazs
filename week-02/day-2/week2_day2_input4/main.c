0c0d1eeac1a4f15905e85b1a5e57ffef676637be
geobalazs <balazskissgeo@gmail.com>
2017-10-10 15:59:16 +0200

    the problem caused by the using of gets after scanf solved week2_day2_input4

--- week-02/day-2/week2_day2_input4/main.c
+++ week-02/day-2/week2_day2_input4/main.c
@@ -2,14 +2,15 @@
 #include <string.h>
 
 int num = 0;
-char name[100];
+char name[100] = {0};
+char enter [1] = {0};
 void func();
 
 int main()
 {
     //write a void function which asks for a number with scanf and print it out with printf,
     //then asks for a name with gets, and print it out with puts
-    //you will notice something is wrong
+    //you will notice something is wrong //<<Balazs: the enter after scanf is passed to gets and user can not input more, after scanf i added the enter to the an enter variable with gets
     //try to solve the problem
 
     func();
@@ -20,8 +21,10 @@ int main()
 void func(){
         printf("Please type a number:\n");
         scanf("%d" , &num);
-        printf("Your number is: %d\n" , num);
-        printf("Please type Your name:\n");
+        printf("Your number is: %d" , num);
+        gets(enter);
+        //getchar();
+        printf("\nPlease type Your name:\n");
         gets(name);
         puts(name);
 }
