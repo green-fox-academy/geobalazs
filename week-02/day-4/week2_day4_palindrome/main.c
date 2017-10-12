1c15b8c73915ac62f23eaa718d7186d2f6d38b57
geobalazs <balazskissgeo@gmail.com>
2017-10-12 16:03:47 +0200

    palindrome output style fixed

--- week-02/day-4/week2_day4_palindrome/main.c
+++ week-02/day-4/week2_day4_palindrome/main.c
@@ -11,16 +11,15 @@ void palindrome(){
 
     int str_l = strlen(str);
 
-    for(int i = 0 ; i <= str_l ; i++){
+    for(int i = 0 ; i <= str_l-1 ; i++){
 
-        printf("'%c" , str[i]);
+        printf("%c" , str[i]);
     }
 
-    printf("\n");
 
     for(int i = str_l-1 ; i >= 0 ; i--){
 
-        printf("'%c" , str[i]);
+        printf("%c" , str[i]);
 
     }
 
