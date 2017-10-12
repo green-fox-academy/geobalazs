d7155ee0287759bce8475d04a62415726c650522
geobalazs <balazskissgeo@gmail.com>
2017-10-12 22:46:31 +0200

    guess number function body structure and statemnets are written

--- week-02/day-4/week2_day4_guessnumber/main.c
+++ week-02/day-4/week2_day4_guessnumber/main.c
@@ -1,21 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int range[100] = {0};
+int guess_number(){
 
     int range_min = 0;
-    int range_max = 0;
+    int range_max = 100;
 
-    printf("Give your range from 0 - 10\n");
+    int guess1 = 0;
+    int guess2 = 0;
+    int guess3 = 0;
+
+    printf("Give your range from 0 - 100\n");
     scanf("%d" , &range_min);
     scanf("%d" , &range_max);
 
-    for(int i = range_min ; i <= range_max ; i++){
-        range[i] = i;
-        printf("%d\t" , range[i]);
+    int rand_num = rand() % 100;
+
+    printf("%d" , rand_num);
+
+    printf("Give your guess:");
+    scanf("%d" , &guess1);
+
+    if(rand_num == guess1){
+        printf("You win!");
+    break;
     }
+
+    printf("Give your guess:");
+    scanf("%d" , &guess2);
+
+    if( rand_num == guess2){
+        printf("You win!");
+    break;
+    }
+
+    printf("Give your guess:");
+    scanf("%d" , &guess3);
+
+    if( rand_num == guess3){
+        printf("You win!");
+    break;
+    }
+    printf("Try again!");
+
+return 0;
+}
+int main()
+{
+
+    guess_number();
 
 return ;
 }
