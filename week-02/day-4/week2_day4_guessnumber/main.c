e790a8f697b6eb65678e1898d1dad4e8058c1031
geobalazs <balazskissgeo@gmail.com>
2017-10-12 23:07:14 +0200

    guess number function with loop ready break should be fixed

--- week-02/day-4/week2_day4_guessnumber/main.c
+++ week-02/day-4/week2_day4_guessnumber/main.c
@@ -6,32 +6,41 @@ int guess_number(){
     int range_min = 0;
     int range_max = 100;
 
-    int guess1 = 0;
-    int guess2 = 0;
-    int guess3 = 0;
+    int guess = 0;
+    //int guess2 = 0;
+    //int guess3 = 0;
 
-    printf("Give your range from 0 - 100\n");
-    scanf("%d" , &range_min);
-    scanf("%d" , &range_max);
+    //printf("Give your range from 0 - 100\n");
+    //scanf("%d" , &range_min);
+    //scanf("%d" , &range_max);
 
     int rand_num = rand() % 100;
 
+    int i = 0;
+    int j = 3;
+
     printf("%d" , rand_num);
 
-    printf("Give your guess:");
-    scanf("%d" , &guess1);
+    do{
+        printf("You have %d try!\nGive your guess:" , j);
+        scanf("%d" , &guess);
+        i++;
+        j--;
+    }while(i <= 2);
+      if (rand_num == guess){
+        break;
+      } printf("Try again!");
 
-    if(rand_num == guess1){
+    printf("You win!");
+
+
+    /*if(rand_num == guess1){
         printf("You win!");
-    break;
-    }
-
-    printf("Give your guess:");
-    scanf("%d" , &guess2);
-
-    if( rand_num == guess2){
+    } else if( rand_num == guess2){
+        printf("Give your guess:");
+        scanf("%d" , &guess2);
+    } {
         printf("You win!");
-    break;
     }
 
     printf("Give your guess:");
@@ -39,9 +48,8 @@ int guess_number(){
 
     if( rand_num == guess3){
         printf("You win!");
-    break;
     }
-    printf("Try again!");
+    printf("Try again!");*/
 
 return 0;
 }
