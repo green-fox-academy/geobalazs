0bb75d38ea65b3fd0339fd84928af84f9ed54ef7
geobalazs <balazskissgeo@gmail.com>
2017-10-17 13:07:12 +0200

    print out the value of pi, first as a string is ready

---
+++ week-03/day-2/week3_day2_e04/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	float val;
+	char str[5];
+	strcpy(str, "3.14");
+
+	printf("%s", str);
+
+	//TODO: print out the value of pi, first as a string, then a float value.
+
+	return(0);
+}
