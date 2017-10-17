022d0a915c44b6794bf54177812d32719b991090
geobalazs <balazskissgeo@gmail.com>
2017-10-17 13:01:02 +0200

    int to str conversation is ready

--- week-03/day-2/week3_day_2_e03_2ndvers/main.c
+++ week-03/day-2/week3_day_2_e03_2ndvers/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+    //TODO: make an integer to string conversion with itoa(), first in binary, then decimal format
+    // Store the string in buffer
+
 
 int main()
 {
-    printf("Hello world!\n");
+	int a = 54325;
+    char buffer[20];
+
+    printf("Binary value: %s\n" , itoa( a , buffer , 2));
+
+    printf("Decimal value: %s\n ", itoa( a , buffer , 10));
+
+    for (int i = 1 ; i < 20 ; i++){
+
+        printf("%c" , buffer[i]);
+    }
+
     return 0;
 }
