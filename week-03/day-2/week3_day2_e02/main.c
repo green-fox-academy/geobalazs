022f790ad40683407d8d65bf4d35a5c5bf9ccad9
geobalazs <balazskissgeo@gmail.com>
2017-10-17 11:25:13 +0200

    small program wich adds 1 to x is fixed

--- week-03/day-2/week3_day2_e02/main.c
+++ week-03/day-2/week3_day2_e02/main.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+
+   //TODO: write a program wich adds up x and 1, and print out it rounded(ill. tizedes jegy nelkul?).
+
 
 int main()
 {
-    printf("Hello world!\n");
+	double x = 1.2;
+
+	double y = x + 1;
+
+	printf ("%d" , (int)y);
+
     return 0;
 }
