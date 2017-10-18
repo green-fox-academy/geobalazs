d08f04a7f8d1c78c1f2f5225df1f4a82d89ef3d8
geobalazs <balazskissgeo@gmail.com>
2017-10-18 14:00:24 +0200

    calculator start up msg ready

---
+++ week-03/day-3/calculator/startup/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("          CLI Calculator\n");
+    printf("====================================\n");
+    printf("usage: [number] [operation] [number]\n");
+    printf("Commands:\n");
+    printf(" +	summation");
+    printf(" -	subtraction\n");
+    printf(" *	multiplication\n");
+    printf(" /	division\n");
+    printf(" %	division with remainder\n");
+    printf(" ^	squaring\n");
+    printf(" <	square root\n");
+    printf(" log	logarithm\n");
+    printf(" binto	binary to hex or dec\n");
+    printf(" hexto	hexadecimal to bin or dec\n");
+    printf(" decto	decimal to bin or hex\n");
+    printf(" ====================================\n");
+    printf(" exit	exiting from the program\n");
+    printf(" clear	clear the screen\n");
+    printf(" help	print usage\n");
+    printf(" ====================================\n");
+    printf(" Hit enter to start!\n");
+    printf(" ====================================\n");
+
+    return 0;
+}
