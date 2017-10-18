4254f6791876429ea4e919a10f38c5bca6e83fde
geobalazs <balazskissgeo@gmail.com>
2017-10-18 14:51:19 +0200

    division to calculator

---
+++ week-03/day-3/calculator/division/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int division(int a , int b){
+
+    int result = 0;
+    result = a / b;
+    return result;
+
+}
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    int sum = 0;
+
+    printf("Enters two numbers: ");
+    scanf("%d %d" , &num1 , &num2);
+
+    sum = division(num1 , num2);
+
+    printf("sum = %d" , sum);
+
+    return 0;
+}
