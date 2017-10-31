45580454daa15d33c9b6b66a4482ff543fcef519
geobalazs <balazskissgeo@gmail.com>
2017-10-31 17:06:23 +0100

    the code is implemented in a function

--- week-04/day-2/week4_day2_e17/main.c
+++ week-04/day-2/week4_day2_e17/main.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 #define MAX_CHARS 255 // Maximum characters allowed
 
-//char frequency(char *array, int max_occurence){
+ char frequency(char *array , int *max_occurence){
 
-//    return ;
-//}
+    char temp;
+    int i, j;
+    int str_length  = strlen(array);
+    int occurence = 1;
+    *max_occurence = 0;
+    char freq_char;
+
+    for (i = 0; i < str_length-1; i++) {
+      for (j = i+1; j < str_length; j++) {
+         if (array[i] > array[j]) {
+            temp  = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+         }
+      }
+    }
+    printf("%s\n\n" , array);
+
+    for( i = 0 ; i < str_length ; i++){
+
+        if(array[i] != array[i + 1]){
+
+           if(occurence > *max_occurence){
+            *max_occurence = occurence;
+            freq_char = array[i];
+           }
+           occurence = 1;
+        }
+
+        if( array[i] == array[i + 1]){
+            occurence++;
+            printf("%d  %c\n" , occurence , array[i]);
+
+        }
+    }
+    printf("-%d- , %c" , *max_occurence , freq_char);
+
+    return freq_char;
+}
 
 int main()
 {
@@ -13,46 +50,12 @@ int main()
     //the number of occurrence should be an output parameter
 
     char str[] = "e is the most frequent element.";
-    char temp;
-    int i, j;
-    int str_length  = strlen(str);
-    int occurence = 1;
-    int max_occurence = 0;
-    char freq_char[1];
-    //printf("%c" , freq_char[0]);
+    int occurence_number = 0;
 
-    for (i = 0; i < str_length-1; i++) {
-      for (j = i+1; j < str_length; j++) {
-         if (str[i] > str[j]) {
-            temp  = str[i];
-            str[i] = str[j];
-            str[j] = temp;
-         }
-      }
-    }
-    printf("%s\n\n" , str);
+    char ferq_letter = frequency(str , &occurence_number);
 
-    for( i = 0 ; i < str_length ; i++){
+    printf("\nThe most freq char: %c , its occurence: %d\n" , ferq_letter , occurence_number);
 
-        if(str[i] != str[i + 1]){
-
-           if(occurence > max_occurence){
-            max_occurence = occurence;
-            freq_char[0] = str[i];
-           }
-           occurence = 1;
-        }
-
-        if( str[i] == str[i + 1]){
-            occurence++;
-            printf("%d  %c\n" , occurence , str[i]);
-
-            //if (str[i] != str[i + 1] && actual_length > most_length){
-                   // most_length = actual_length;
-               // }
-        }
-    }
-    printf("-%d- , %c" , max_occurence , freq_char[0]);
 //    printf("\n\n%d" , actual_length);
 
 
