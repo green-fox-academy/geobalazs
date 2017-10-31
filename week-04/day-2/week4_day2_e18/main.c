ff3be38cd920127806ff3d35a7c4c528ac81de52
geobalazs <balazskissgeo@gmail.com>
2017-10-31 17:55:35 +0100

    the code is implemented into a function

--- week-04/day-2/week4_day2_e18/main.c
+++ week-04/day-2/week4_day2_e18/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int first_word(char *string, char *first);
+int first_word(char *string, char *first){
+
+    //char first[50];
+    int i = 0;
+
+    for( i=0 ; string[i] != ' ' ; i++ ){
+        first[i] = string[i];
+       // printf("%c" , first[i]);
+        first[i + 1] = '\0';
+    }
+
+    int size = strlen(first);
+    //printf("%d" , size);
+    //printf("%s" , first);
+
+return size;
+}
 
 int main()
 {
@@ -9,15 +25,8 @@ int main()
 
     char str[] = "Extracting the first word.";
     char first[50];
-    int i = 0;
 
-    for( i=0 ; str[i] != ' ' ; i++ ){
-        first[i] = str[i];
-       // printf("%c" , first[i]);
-        first[i + 1] = '\0';
-    }
-
-    printf("%s" , first);
+    printf("%s , %d" , first , first_word(str , first));
 
     return 0;
 }
