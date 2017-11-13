4ee62c576823d74559e2c1540a09ee049df4de41
geobalazs <balazskissgeo@gmail.com>
2017-11-13 14:43:32 +0100

    variables declared and initialized, if else statemetn to print out marriage info is written

--- week-06/day-01/task_datatype_01/main.cpp
+++ week-06/day-01/task_datatype_01/main.cpp
@@ -16,11 +16,17 @@ int main()
 
     int coding_time = 6; // hours / student / day
     int semester_long = 17; //weeks
-    int days = 5;
+    int days = 5; //workdays / week
+    int work_hours = 52; //average work hours in the semester
 
-    int full_coding_time = semester_long * coding_time * days;
+    int full_coding_time = semester_long * coding_time * days; //hours is spent with coding in a semester by an attendee
+    int full_working_time = work_hours * semester_long; // working time of the semester
+    double percentage_semester_coding_hours = (double) full_coding_time / full_working_time * 100; //
+
+
 
     cout << full_coding_time << " hours is spent with coding in a semester by an attendee" << endl;
+    cout << percentage_semester_coding_hours << " percentage of the coding hours in the semester" << endl;
 
 
     return 0;
