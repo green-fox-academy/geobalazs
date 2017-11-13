2b6ccd6fa038d7ec01e70f8da9a75f38df59bd25
geobalazs <balazskissgeo@gmail.com>
2017-11-13 14:46:37 +0100

    consol out lines are ready

--- week-06/day-01/task_datatype_02/main.cpp
+++ week-06/day-01/task_datatype_02/main.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    string name = "Balazs Kiss";
+    int age = 33;
+    double height = 1.75;
+    bool married = 0;
+    string marriage = "";
+
+    if(married){
+        marriage = "married.";
+    } else {
+        marriage = "not married.";
+    }
+
+    cout << "My name: " << name << endl;
+    cout << "My age: "<< age << endl;
+    cout << "My height: "<< height << endl;
+    cout << "I am " << marriage << endl;
+
     return 0;
 }
