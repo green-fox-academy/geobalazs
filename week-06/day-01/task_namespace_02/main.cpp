d48dc8edb578f281336fe13e946af4cd8934a9b9
geobalazs <balazskissgeo@gmail.com>
2017-11-13 15:00:26 +0100

    person namespace declared initialized

---
+++ week-06/day-01/task_namespace_02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+namespace PERSON {
+    string name = "geobalazs";
+    int age = 33;
+    int fav_num = 3;
+}
+
+// Create your own namespace, called Person
+// store your name as a string
+// your age as an integer
+// and your favorite number as an integer
+// write them out in main()
+
+int main()
+{
+
+    return 0;
+}
