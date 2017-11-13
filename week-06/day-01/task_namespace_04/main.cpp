0b448bb986b8557d688929e2aa8fab569fbb65f5
geobalazs <balazskissgeo@gmail.com>
2017-11-13 15:25:48 +0100

    namespaces are declared and initialized and called with the using directive

---
+++ week-06/day-01/task_namespace_04/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// Create your own namespace, called Mark
+// store your name as a string
+// your age as an integer
+// and your favorite number as an integer
+//
+
+namespace Mark {
+    string name_1 = Mark;
+    int age_1 = 20;
+    int fav_num_1 = 45;
+}
+
+// Create your own namespace, called Maria
+// store your name as a string
+// your age as an integer
+// and your favorite number as an integer
+//
+namespace Maria {
+    string name_2 = Maria;
+    int age_2 = 15;
+    int fav_num_2 = 90;
+}
+// create a function that can add the two age, and returns it
+
+
+
+int main()
+{
+    using namespace Mark;
+    using namespace Maria;
+
+
+    return 0;
+}
