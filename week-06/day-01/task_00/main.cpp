41b6cd8024725a3f60b1b2300504d72ffd683517
geobalazs <balazskissgeo@gmail.com>
2017-11-13 11:37:39 +0100

    my 1st c++ code

---
+++ week-06/day-01/task_00/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// Modify this program to greet you instead of the World!
+
+int main()
+{
+    string name = "Balazs";
+    cout << "Hello " << name << "!" << endl;
+    return 0;
+}
