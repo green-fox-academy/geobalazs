674520941ef0032a807f4680df65573a6b220d3f
geobalazs <balazskissgeo@gmail.com>
2017-11-14 14:41:49 +0100

    car class syntax ready and its body filled with private variable car type and car km, public constructor takes these variables as parameters are and constructor body is ready

---
+++ week-06/day-02/task_06/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+    // Create a class called "Car"
+    // It should have a "type" property that stores the car's type in a string eg: "Mazda"
+    // It should have a "km" property that stores how many kilometers it have run
+    // The km and the type property should be a parmeter in the constructor
+    // It should have a method called "run" that takes a number and increments the "km" property by it
+
+using namespace std;
+
+class Car{
+
+    private:
+        string type;
+        float km;
+
+    public:
+        Car(string type , float km){
+            this -> type = type;
+            this -> km = km;
+        }
+} ;
+
+int main() {
+
+  return 0;
+}
