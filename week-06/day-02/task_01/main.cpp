feaa2875a3a9745515562133fb023185bec7b9ff
geobalazs <balazskissgeo@gmail.com>
2017-11-14 11:39:18 +0100

    main is fixed, object is declared with parameters for the constructor and print data function is called

--- week-06/day-02/task_01/main.cpp
+++ week-06/day-02/task_01/main.cpp
@@ -23,7 +23,7 @@ int main() {
   // Create a new instace of the Student class and set it's name to "John" and
   // it's age to 21
 
-    Student so; // objektum letrehozasa
-
+    Student so("John" , 21); // objektum letrehozasa
+    so.data_print();
   return 0;
 }
