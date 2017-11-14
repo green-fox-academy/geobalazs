95f7cf1025fbc06c1838b96c4ccaeaef17a02ad2
geobalazs <balazskissgeo@gmail.com>
2017-11-14 14:04:09 +0100

    get_price method works

--- week-06/day-02/task_04/main.cpp
+++ week-06/day-02/task_04/main.cpp
@@ -20,7 +20,8 @@ class House {
     }
 
     unsigned int get_price() {
-      return this->price;
+
+      return this->price = area * 400;
     }
 };
 
@@ -28,6 +29,7 @@ int main() {
   // The market price of the houses is 400 EUR / square meters
   // Make the get_price() function work
   // Create a constructor for the House class that takes it's address and area.
+
   House house = House("Andrassy 66", 349);
   cout << house.get_price();
 
