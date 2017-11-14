23b4a80fbea6851d2f77ddf44e79a07ed56e6c63
geobalazs <balazskissgeo@gmail.com>
2017-11-14 15:01:42 +0100

    run method body and parameters ready, function calls in main, my_print method added

--- week-06/day-02/task_06/main.cpp
+++ week-06/day-02/task_06/main.cpp
@@ -14,17 +14,29 @@ class Car{
     private:
         string type;
         float km;
+        float new_km;
 
     public:
         Car(string type , float km){
             this -> type = type;
             this -> km = km;
         }
+    void run(float new_km){
+        this -> km = km + new_km;
+    }
+
+    void my_print(){
+        cout << km << endl;
+    }
 } ;
 
 int main() {
 
     Car co("Ford" , 120);
+
+    co.run(10);
+
+    co.my_print();
 
   return 0;
 }
