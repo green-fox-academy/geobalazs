b90c8c05b5bed6bbc59e90a7a595d5185ef62e5c
geobalazs <balazskissgeo@gmail.com>
2017-11-15 13:48:41 +0100

    methods are implemented

--- week-06/day-03/task01/main.cpp
+++ week-06/day-03/task01/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+// Create a Shape class, inherit two derived class from it
+// One should be Circle, and the other one is Triangle
+// For each derived class, implement a calc_area() method
+// which calculates the area of the shape and stores it inside
+// the class (so it is a private field)
+
+class Shape {
+
+    public:
+        virtual void calc_area() = 0;
+
+};
+
+class Circle: Shape {
+
+    private:
+        float area;
+
+    public:
+        void calc_area(float radius){
+            area = radius * radius * 2;
+        }
+};
+
+class Triangle: Shape{
+
+    private:
+        float area;
+
+    public:
+        void calc_area(float base, float height){
+            area = base * height / 2;
+        }
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
