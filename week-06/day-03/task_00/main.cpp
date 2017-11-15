37c62bed2f75960a29ba9e1c2a3a54d0aae7315a
geobalazs <balazskissgeo@gmail.com>
2017-11-15 13:20:50 +0100

    derived class declaration problem fixed, travelling function is impelemented

--- week-06/day-03/task_00/main.cpp
+++ week-06/day-03/task_00/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+// Create a Human class which is a base class for
+// SuperHuman and AquaHuman
+// Create a virtual function in Human class called travelling()
+// It should print out different ways to travel
+// For example: Human is walking. SuperHuman is flying. AquaHuman is riding with dolphins.
+
+class Human {
+    public:
+        virtual void travelling(){
+            cout << "walking DEATH" << endl;
+        }
+};
+
+class SuperMan: Human{
+    public:
+        void travelling(){
+            cout << "Fly on your way, like an eagle"
+                 << "Fly as high as the sun"
+                 << "On your way like an eagle"
+                 << "Fly and touch the sun"
+                 << endl;
+        }
+};
+
+class AquaMan: Human{
+    public:
+        void travelling(){
+            cout << "Riding with dolphins" << endl;
+        }
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
     return 0;
 }
