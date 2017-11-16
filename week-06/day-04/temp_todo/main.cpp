c914ff8aa675a14f929588d228ef6f1cb14dd073
geobalazs <balazskissgeo@gmail.com>
2017-11-16 17:33:34 +0100

    add new task by

--- week-06/day-04/temp_todo/main.cpp
+++ week-06/day-04/temp_todo/main.cpp
@@ -3,22 +3,35 @@
 
 using namespace std;
 
+void add_new_task();
+
 int main()
 {
+
     vector<string> taskVector;
     string command = "";
     string task = "";
 
-    cin >> command >> task;
-    taskVector.push_back(task);
+    cin >> command;
+    if (command == "-a"){
+       //cout << command;
+       add_new_task();
+    }
+
+    //taskVector.push_back(task);
     //cout << command;
     //cout << taskVector[0];
-    cout << "add new task" << endl;
-    cin >> command >> task;
-    taskVector.push_back(task);
-    cout << taskVector[0] << taskVector[1];
+    //cout << taskVector[0] << taskVector[1];
 
 
     return 0;
 
 }
+
+void add_new_task(){
+    //cout << "hello";
+    cin >> task;
+    taskVector.push_back(task);
+    cout << taskVector[0];
+    }
+
