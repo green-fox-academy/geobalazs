8d4bf287f9fc8715019bf33041d102c6852231d6
geobalazs <balazskissgeo@gmail.com>
2017-11-22 16:53:50 +0100

    new concept

--- week-07/day-01/task_13/main.cpp
+++ week-07/day-01/task_13/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <ctime>
 #include <vector>
+#include <chrono>
+#include <thread>
+#include <windows.h>
 
 using namespace std;
 
@@ -10,22 +12,38 @@ using namespace std;
 // the program should print out: "faild, it took you too much time"
 // This program cloud be the engine of a quiz game.
 
-void user_input(){
 
-    int i = 0;
-    int number =0;
-    vector <int> NumberVector;
-
-    cout << "You have 10 sec to give 10 number:" << endl;
-    for (i = 0 ; i < 5 ; ++i ){
-    cin >> number;
-    NumberVector.push_back(number);
-    }
-}
 
 int main() {
 
-    user_input();
-    sleep(1000);
-    system (cls);
+    int i = 0;
+    int number =0;
+    //vector <int> NumberVector;
+
+    for(i = 10 ; i > 0 ; --i){
+       cout << i << endl;
+       this_thread::sleep_for(chrono::seconds(1));
+       system ("cls");
+    }
+    cout << "boom";
+    //try {
+    //       if (i <= 0){
+    //           throw "Failed, it took you too much time...";
+
+    //           cin >> number;
+    //           NumberVector.push_back(number);
+    //       }
+    //   }
+    //   catch (const char *err_msg){
+    //       cout << err_msg << endl;
+    //   }
+
+    //cout << "You have 5 sec to give 5 number:" << endl;
+    //cin >> number;
+    //NumberVector.push_back(number);
+    //Sleep(5000);
+    //cout << i << endl;
+    //cout << "Failed, it took you too much time...";
+
+  return 0;
 }
